19809579d692a1b1915d058c3b2dec96c0aead17
Arthur Allshire <arthur@allshire.org>
2019-05-25 15:48:59 -0400

    Plot errors between analytic and qr legendre polynomials

--- legendre.py
+++ legendre.py
@@ -2,12 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def approximate_legendre(n, samples=1000):
+def legendre_analytic_order_3(x):
+    return [1, x, 0.5*(3*x**2 - 1), 0.5*(5*x**3-3*x)]
+
+
+def approximate_legendre(n, samples=257):
     x = np.linspace(-1., 1., samples).reshape(-1, 1)
 
-    A = np.concatenate([x**p for p in range(n)], axis=1)
+    A = np.concatenate([x**p for p in range(n+1)], axis=1)
 
     Q, _ = np.linalg.qr(A)
+
+    # legendre polynomials satisfy P_k(1) = 1
+    Q = Q @ np.diag(1 / Q[-1, :])
 
     return x, Q
 
@@ -18,11 +25,22 @@ def plot_columns(x, mat):
     plt.show()
 
 
-x, legendre_matrix = approximate_legendre(4)
-plot_columns(x, legendre_matrix)
+def legendre_errors(x, mat):
+    analytical = []
+    for x_ in x.reshape(-1):
+        analytical.append(legendre_analytic_order_3(x_))
+    print(analytical[:5])
+    diff = np.array(analytical) - mat
+    return diff
 
 
-# inner product should be approx 0
-# high samples are discrete analogue of integral between -1, 1
-# between two polynomials in L^2[-1, 1]
-print(legendre_matrix[:, 0] @ legendre_matrix[:, 1])
+if __name__ == "__main__":
+    x, legendre_qr = approximate_legendre(3)
+    print(legendre_qr[:5])
+    errors = legendre_errors(x, legendre_qr)
+    plot_columns(x, errors)
+
+    # inner product should be approx 0
+    # high samples are discrete analogue of integral between -1, 1
+    # between two polynomials in L^2[-1, 1]
+    print(legendre_qr[:, 0] @ legendre_qr[:, 1])
